3de086aeb4516316ea57d7eb57e667ca39ab1d05
paichiwo <lzerucha@gmail.com>
2024-05-07 09:21:56 +0100

    Remove format_title_string as not necessary

--- helpers.py
+++ helpers.py
@@ -1,18 +1,10 @@
 from moviepy.editor import AudioFileClip
 from datetime import datetime
-import re
-
-
-def format_title_string(title):
-    # Remove any special characters in the title string leaving only alphanumeric and spaces
-    formatted_title = re.sub(r'[^\w ]', '', title)
-    return formatted_title
 
 
 def format_date_string(result):
-    # Format date string to DD-MM-YYYY, HH:MM:SS format
-    date_fetched = result[0].text_content()
-    date_stripped = date_fetched[20:-10] + date_fetched[-8:-2]
+    # Format date string to DD-MM-YYYY HH:MM:SS format
+    date_stripped = result[20:-10] + result[-8:-2]
     date = datetime.strptime(date_stripped, '%H:%M %Z on %B %d, %Y').strftime('%d-%m-%Y %H:%M:%S')
     return date
 
