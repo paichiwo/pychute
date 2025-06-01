edcff4517aa4229e4721c8b3853dfc9045c2ce34
paichiwo <lzerucha@gmail.com>
2025-06-01 13:13:39 +0100

    Update to 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='pychute',
-    version='0.1.4',
+    version='0.1.5',
     packages=setuptools.find_packages(),
     url='http://github.com/paichiwo/pychute',
     author='Lukasz Zerucha',
